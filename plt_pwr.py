c184ab80567597d076d3e89da39454a88c27e54a
ignasipc <ignasipc22@gmail.com>
2025-12-19 14:24:53 +0100

    Añadidas los nuevos tests y arreglados algunos errores

--- plt_pwr.py
+++ plt_pwr.py
@@ -2,31 +2,33 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def main():
-    name_hw = 'test1_ignasi'   # Nombre del archivo del monitor Hardware
-    name_sw = 'gpu_log'          # Nombne del archivo del monitor Software
+    name_hw = 'consumo_pc_19_12_2025.txt'   # Nombre del archivo del monitor Hardware
+    name_sw = 'gpu_log_19_12_2025.csv'          # Nombne del archivo del monitor Software
 
     # Cargar el archivo
     df_hw = pd.read_csv(
-        "Monitoring_logs/%s.txt" % name_hw,
+        "Monitoring_logs/%s" % name_hw,
         sep="\t",  # Separación por tabulación
         decimal=",",  # Convierte comas a decimales reales
         engine="python"  # Permite separadores irregulares
     )
 
-    df_sw = pd.read_csv("Monitoring_logs/%s.csv" % name_sw)
+    df_sw = pd.read_csv("Monitoring_logs/%s" % name_sw)
 
     # Convertir W a número y quitar signo negativo
     df_hw["W"] = df_hw["W"].astype(float).abs()
     df_sw["W"] = df_sw[" power.draw [W]"].str.replace("W", "").astype(float)
 
+    # Convertir porcentaje de uso de GPU a número
+    df_sw["uso_gpu"] = df_sw[" utilization.gpu [%]"].str.replace("%", "").astype(float)
+
     # Crear eje de tiempo en segundos: 1, 2, 3...
     df_hw["t_seconds"] = range(1, len(df_hw) + 1)
     df_sw["t_seconds"] = range(1, len(df_sw) + 1)
 
-    print(f"\nDuración total de la prueba Hardware: {len(df_hw)}s")
-    print(f"Duración total de la prueba Software: {len(df_sw)}s")
-    print(f"\nDuración total formateada Hardware (HH:MM:SS): {len(df_hw)// 3600:02d}:{(len(df_hw) % 3600) // 60:02d}:{len(df_hw)% 60:02d}")
-    print(f"Duración total formateada Hardware (HH:MM:SS): {len(df_sw)// 3600:02d}:{(len(df_sw) % 3600) // 60:02d}:{len(df_sw)% 60:02d}")
+    # Imprimir la duración de ambas pruebas
+    print(f"\nDuración total de la prueba Hardware: {len(df_hw)}s - Formato (HH:MM:SS): {len(df_hw)// 3600:02d}:{(len(df_hw) % 3600) // 60:02d}:{len(df_hw)% 60:02d}")
+    print(f"Duración total de la prueba Software: {len(df_sw)}s - Formato (HH:MM:SS): {len(df_sw)// 3600:02d}:{(len(df_sw) % 3600) // 60:02d}:{len(df_sw)% 60:02d}")
 
     # Graficamos los resultados del monitor HW y SW (W)
     plt.figure(figsize=(10, 5))
@@ -43,7 +45,7 @@ def main():
 
     # Graficamos los resultados del monitor SW (% de uso)
     plt.figure(figsize=(10, 5))
-    plt.plot(df_sw["t_seconds"], df_sw[" utilization.gpu [%]"], linewidth=1.5, color="")
+    plt.plot(df_sw["t_seconds"], df_sw["uso_gpu"], linewidth=1.5, color="blue")
     plt.xlabel("Tiempo (s)")
     plt.ylabel("Uso de GPU (%)")
     plt.title("Porcentaje de uso de la GPU")
