244ebbd67fbb09d280de85dd775602e8f2964823
ignasipc <ignasipc22@gmail.com>
2025-12-19 11:59:20 +0100

    Dibujado del consumo de ambos monitores en el mismo gráfico y creada una nueva carpeta para almacenar los datos de los monitores

--- plt_pwr.py
+++ plt_pwr.py
@@ -7,16 +7,17 @@ def main():
 
     # Cargar el archivo
     df_hw = pd.read_csv(
-        "%s.txt" % name_hw,
+        "Monitoring_logs/%s.txt" % name_hw,
         sep="\t",  # Separación por tabulación
         decimal=",",  # Convierte comas a decimales reales
         engine="python"  # Permite separadores irregulares
     )
 
-    df_sw = pd.read_csv("%s.csv" % name_sw)
+    df_sw = pd.read_csv("Monitoring_logs/%s.csv" % name_sw)
 
     # Convertir W a número y quitar signo negativo
     df_hw["W"] = df_hw["W"].astype(float).abs()
+    df_sw["W"] = df_sw[" power.draw [W]"].str.replace("W", "").astype(float)
 
     # Crear eje de tiempo en segundos: 1, 2, 3...
     df_hw["t_seconds"] = range(1, len(df_hw) + 1)
@@ -27,26 +28,28 @@ def main():
     print(f"\nDuración total formateada Hardware (HH:MM:SS): {len(df_hw)// 3600:02d}:{(len(df_hw) % 3600) // 60:02d}:{len(df_hw)% 60:02d}")
     print(f"Duración total formateada Hardware (HH:MM:SS): {len(df_sw)// 3600:02d}:{(len(df_sw) % 3600) // 60:02d}:{len(df_sw)% 60:02d}")
 
-    # Graficamos los resultados del monitor SW
+    # Graficamos los resultados del monitor HW y SW (W)
     plt.figure(figsize=(10, 5))
-    plt.plot(df_hw["t_seconds"], df_hw["W"], linewidth=1.5)
+    plt.plot(df_hw["t_seconds"], df_hw["W"], linewidth=1.5, color="red", label="Consumo total (Monitor HW)")
+    plt.plot(df_sw["t_seconds"], df_sw["W"], linewidth=1.5, color="green", label="Consumo de la GPU (Monitor SW)")
+    plt.legend()
     plt.xlabel("Tiempo (s)")
     plt.ylabel("Watios (W)")
     plt.title("Consumo de Watios a lo largo del tiempo (Monitor SW)")
     plt.grid(True)
     plt.tight_layout()
-    plt.savefig('Plots/Plot_%s.png' % name_hw)
+    plt.savefig('Plots/Plot_power_consumption.png')
     plt.show()
 
-    # Graficamos los resultados del monitor HW
+    # Graficamos los resultados del monitor SW (% de uso)
     plt.figure(figsize=(10, 5))
-    plt.plot(df_sw["t_seconds"], df_sw[" utilization.gpu [%]"], linewidth=1.5)
+    plt.plot(df_sw["t_seconds"], df_sw[" utilization.gpu [%]"], linewidth=1.5, color="")
     plt.xlabel("Tiempo (s)")
     plt.ylabel("Uso de GPU (%)")
     plt.title("Porcentaje de uso de la GPU")
     plt.grid(True)
     plt.tight_layout()
-    plt.savefig('Plots/Plot_%s.png' % name_sw)
+    plt.savefig('Plots/Plot_GPU_usage.png')
     plt.show()
 
 
